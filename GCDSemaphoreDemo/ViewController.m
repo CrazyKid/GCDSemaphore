cc68a841beb5d0dbfc1ffecedd26f036b3cd9696
Arthur <jjyy1106@qq.com>
2016-08-24 09:57:10 +0800

    demo

--- GCDSemaphoreDemo/ViewController.m
+++ GCDSemaphoreDemo/ViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
+#define kUrlString @"http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json"    //新浪接口
 @interface ViewController ()
 
 @end
@@ -17,6 +19,78 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    [self requestUsingGroup];
+    [self requestUsingSemaphore];
+}
+
+- (void)requestUsingGroup {
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    //request 1
+    dispatch_group_async(group, queue, ^{
+        [[AFHTTPSessionManager manager] POST:kUrlString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            
+            NSLog(@"the first request responseObject:%@",responseObject);
+            
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            NSLog(@"error:%@",error.localizedDescription);
+        }];
+    });
+    //request 2
+    dispatch_group_async(group, queue, ^{
+        [[AFHTTPSessionManager manager] POST:kUrlString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            
+            NSLog(@"the second request responseObject:%@",responseObject);
+            
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            NSLog(@"error:%@",error.localizedDescription);
+        }];
+    });
+    //
+    dispatch_group_notify(group, queue, ^{
+        NSLog(@"all requests completed");
+    });
+    
+    /**
+     *  log:
+     *  all requests completed
+     *  the second request responseObject:{data}
+     *  the first request responseObject:{data}
+     */
+    
+}
+
+- (void)requestUsingSemaphore {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+        //request 1
+        [[AFHTTPSessionManager manager] POST:kUrlString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            NSLog(@"the first request responseObject: %@",responseObject);
+            dispatch_semaphore_signal(semaphore);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            NSLog(@"error : %@",error.localizedDescription);
+        }];
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        //request 2
+        [[AFHTTPSessionManager manager] POST:kUrlString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            NSLog(@"the second request responseObject: %@",responseObject);
+            dispatch_semaphore_signal(semaphore);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            NSLog(@"error : %@",error.localizedDescription);
+        }];
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        //
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"all requests completed");
+        });
+    });
+    /**
+     *  log
+     *  the second request responseObject:{data}
+     *  the first request responseObject:{data}
+     *  all requests completed
+     */
 }
 
 - (void)didReceiveMemoryWarning {
